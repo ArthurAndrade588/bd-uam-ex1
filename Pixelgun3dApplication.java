b39b7d71d33d5ec6234d9b43a562402621faf5ea
ArthurAndrade588 <delsinstc@gmail.com>
2026-08-26 09:57:51 -0300

    Add files via upload

---
+++ Pixelgun3dApplication.java
@@ -0,0 +1,13 @@
+package com.uam.bd.pixelgun3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pixelgun3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pixelgun3dApplication.class, args);
+	}
+
+}
---
+++ Pixelgun3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uam.bd.pixelgun3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pixelgun3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
